f4af57dac1a06ab1e2cacc2578552ca20dc249a7
Unknown <dim.meln@yandex.ru>
2019-09-24 00:59:41 +0300

    Project Created

---
+++ LoG/Intermediate/Build/Win64/LoGEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "D:/GIT/LifeOfGame-prot/LoG/Intermediate/Build/Win64/LoGEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ LoG/Source/LoG/GameOfLife.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameOfLife.h"
+
+GameOfLife::GameOfLife()
+{
+}
+
+GameOfLife::~GameOfLife()
+{
+}
