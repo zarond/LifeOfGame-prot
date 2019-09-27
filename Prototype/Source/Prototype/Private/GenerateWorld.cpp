09285bef4b47c55d2a9cdaa06cfacf81bcde8196
zarond <55189420+zarond@users.noreply.github.com>
2019-09-28 00:47:30 +0300

    минорное развитие
    Ничего полезного

---
+++ Prototype/Source/Prototype/Private/GenerateWorld.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GenerateWorld.h"
+
+GenerateWorld::GenerateWorld()
+{
+}
+
+GenerateWorld::~GenerateWorld()
+{
+}
---
+++ Prototype/Source/Prototype/Public/GenerateWorld.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PROTOTYPE_API GenerateWorld
+{
+public:
+	GenerateWorld();
+	~GenerateWorld();
+    void DoSomething(){
+        int i=0;
+    }
+};
