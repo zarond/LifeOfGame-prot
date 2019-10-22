61721753097bc79dac95943b1633fc640bf1e214
Unknown <shostackovich@gmail.com>
2019-10-23 02:50:05 +0300

    Worked on GoL turns
    Just save. It is still not connected with game.

--- Prototype/Source/Prototype/Public/GoLUser.h
+++ Prototype/Source/Prototype/Public/GoLUser.h
@@ -11,18 +11,20 @@ class PROTOTYPE_API AGoLUser : public AActor
 {
 	GENERATED_BODY()
 	
+private:
+	int _width;
+	int _height;
+	TArray<bool> _birth;
+	TArray<bool> _survive;
+	TArray<AActor*> LavaPieces;
+	bool** GoLField;
+	const double birthChance = 0.47;
+	const TArray<bool> defaultBirth = { false, false, false, true, false, false, false, false, false };
+	const TArray<bool> defaultSurvive = { false, false, true, true, false, false, false, false, false };
+
 public:	
 	// Sets default values for this actor's properties
 	AGoLUser();
-	//AGoLUser(const int width, const int height);
-	//~AGoLUser();
-	/*void UpdateWorld();
-	
-
-	GameOfLife* GOL;
-	TArray<AActor*> LavaPieces;
-	int _width;
-	int _height;*/
 
 protected:
 	// Called when the game starts or when spawned
@@ -32,4 +34,12 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+public:
+	~AGoLUser();
+	UFUNCTION(BlueprintCallable, Category = "Game Of Life")
+	void GenerateGoL(int width, int height, TArray<bool> birth = {}, TArray<bool> survive = {});
+	UFUNCTION(BlueprintCallable, Category = "Game Of Life")
+	void UpdateGoL(TArray<bool> birth = {}, TArray<bool> survive = {});
+	UFUNCTION(BlueprintCallable, Category = "Game Of Life")
+	TArray<AActor*> PutLavaPiecesOnField();
 };
